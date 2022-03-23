ad964f0ce4eb386e320dca98dca4a0110a280281
oktaygulmez <oktaybjk97@hotmail.com>
2022-03-23 12:36:26 +0300

    first

---
+++ app/src/main/java/com/oktaygulmez/androidprojem/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.oktaygulmez.androidprojem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun hesapla(view : View){
+        val yakitort = yakitortText.text.toString().toInt()
+        val km = kmText.text.toString().toInt()
+        val yakitfiyat = yakitFiyatText.text.toString().toInt()
+        var sonuc = (yakitort*(km/100)*yakitfiyat)
+        var kmkacyakar = sonuc/km
+        sonucText.text = "Aracınız Toplamda " + sonuc.toString()+" liralık yakıt yakacaktır."
+        kmKacYakarText.text = "Aracınız km başına " + kmkacyakar + " lira yakacaktır."
+    }
+}
